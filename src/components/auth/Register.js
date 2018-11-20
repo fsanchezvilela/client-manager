3e6d2ea320f96f986fb4ac7607ad52e4994681df
Fernando <f.sanchezvilela@gmail.com>
2018-11-20 21:26:07 +0100

    add the Register functionality

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -12,18 +12,25 @@ class Login extends Component {
     password: ''
   };
 
+  //using lifecycle to redirect
+
+  componentWillMount() {
+    const { allowRegistration } = this.props.settings;
+    if (!allowRegistration) {
+      this.props.history.push('/');
+    }
+  }
+
   onSubmit = e => {
     e.preventDefault();
     const { firebase, notifyUser } = this.props;
 
     const { email, password } = this.state;
 
+    // Register with firebase
     firebase
-      .login({
-        email,
-        password
-      })
-      .catch(err => notifyUser('Invalid Login Credentials', 'error'));
+      .createUser({ email, password })
+      .catch(err => notifyUser('That User Already Exists', 'error'));
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -41,7 +48,7 @@ class Login extends Component {
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock" />
-                  Login
+                  Register
                 </span>
               </h1>
               <form onSubmit={this.onSubmit}>
@@ -70,7 +77,7 @@ class Login extends Component {
 
                 <input
                   type="submit"
-                  value="Login"
+                  value="Sign Up"
                   className="btn btn-primary btn-block"
                 />
               </form>
@@ -92,7 +99,8 @@ export default compose(
   firebaseConnect(),
   connect(
     (state, props) => ({
-      notify: state.notify
+      notify: state.notify,
+      settings: state.settings
     }),
     { notifyUser }
   )
